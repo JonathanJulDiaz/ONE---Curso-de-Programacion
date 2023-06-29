30259db3b2b7f710ab8d4c6b8b85f3dee6822fa1
Jonathan Julio <yamidtjulio@hotmail.com>
2023-06-29 18:45:12 -0500

    Codigo para extraer informacion de las pagina de google

--- Challenges/Challenge ONE Conversor de Monedas/src/Main.java
+++ Challenges/Challenge ONE Conversor de Monedas/src/Main.java
@@ -1,5 +1,36 @@
+import java.io.IOException;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String pagina = "https://www.google.com/search?q=cop+a+dolar";
+        //Connecting to the web page
+        Connection conexion = Jsoup.connect(pagina);
+        //executing the get request
+        Document documento = null;
+        try {
+            documento = conexion.get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //Retrieving the contents (body) of the web page
+        String resultado = documento.body().text();
+
+        String[] informacion = resultado.split(" ", -1);
+
+        int contador = 0;
+
+        String[] filtrado = new String[100];
+
+        for(int i = 0; i < filtrado.length; i++) {
+            filtrado[i] = informacion[i];
+        }
+
+        for(String palabra : filtrado){
+            System.out.println(contador++);
+            System.out.println(palabra);
+        }
     }
 }
