34f3d55adda94655a56a635d7831510b941bd290
Jonathan Julio <yamidtjulio@hotmail.com>
2023-06-29 19:43:03 -0500

    Asociamos el convertidor a la pagina Google Finance

--- Challenges/Challenge ONE Conversor de Monedas/src/Main.java
+++ Challenges/Challenge ONE Conversor de Monedas/src/Main.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) {
-        String pagina = "https://www.google.com/search?q=cop+a+dolar";
+        String pagina = "https://www.google.com/finance/quote/COP-USD";
         //Connecting to the web page
         Connection conexion = Jsoup.connect(pagina);
         //executing the get request
@@ -16,21 +19,10 @@ public class Main {
             throw new RuntimeException(e);
         }
         //Retrieving the contents (body) of the web page
-        String resultado = documento.body().text();
+        Element resultado = documento.body();
 
-        String[] informacion = resultado.split(" ", -1);
+        Elements valor = resultado.getElementsByClass("YMlKec fxKbKc");
 
-        int contador = 0;
-
-        String[] filtrado = new String[100];
-
-        for(int i = 0; i < filtrado.length; i++) {
-            filtrado[i] = informacion[i];
-        }
-
-        for(String palabra : filtrado){
-            System.out.println(contador++);
-            System.out.println(palabra);
-        }
+        System.out.println("USD a COP: " + valor.html());
     }
 }
