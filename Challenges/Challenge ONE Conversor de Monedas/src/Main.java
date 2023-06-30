e0657fbdfd4a3665a8df64a4a301789d86c53a37
Jonathan Julio <yamidtjulio@hotmail.com>
2023-06-29 22:35:26 -0500

    Le agregamos el convertidor de divisas, faltando implementar casos de error y el convertidor de temperatura

--- Challenges/Challenge ONE Conversor de Monedas/src/Main.java
+++ Challenges/Challenge ONE Conversor de Monedas/src/Main.java
@@ -5,24 +5,124 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import javax.swing.JOptionPane;
 
 public class Main {
-    public static void main(String[] args) {
-        String pagina = "https://www.google.com/finance/quote/COP-USD";
-        //Connecting to the web page
+
+    public static void inicio(){
+        Object[] listaConvertidor = {"Convertidor de Monedas", "Convertidor de Temperatura"};
+
+        Object opcionesDeConvertidor = JOptionPane.showInputDialog(null,"Seleccione uno de los convertidores", "Convertidores",JOptionPane.INFORMATION_MESSAGE, null, listaConvertidor, listaConvertidor[0]);
+
+        String valor = JOptionPane.showInputDialog("Ingrese el valor a convertir");
+
+        double valorAConvertir = 0.0;
+
+        try {
+            valorAConvertir = Double.parseDouble(valor);
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Ha ocurrido un error", "Error",JOptionPane.ERROR_MESSAGE);
+
+            e.printStackTrace();
+        }
+
+        if(opcionesDeConvertidor.toString() == listaConvertidor[0]){
+            monedas(valorAConvertir);
+        } else {
+            temperatura(valorAConvertir);
+        }
+
+        boolean a = deseaSeguir();
+    }
+
+    private static void monedas(double valor){
+        Object[] conversiones = {"COP a Dólares", "COP a Euros", "COP a Libras Esterlinas", "COP a Yen Japonés", "COP a Won Sur-coreano", "Dólares a COP", "Euros a COP", "Libras Esterlinas a COP", "Yen Japonés a COP", "Won Sur-coreano a COP"};
+
+        Object opcionesDeConvertidor = JOptionPane.showInputDialog(null,"Seleccione a que moneda convertir", "Conversion de Monedas", JOptionPane.INFORMATION_MESSAGE, null, conversiones, conversiones[0]);
+
+        convertir(opcionesDeConvertidor.toString(), valor);
+    }
+
+    private static void convertir(String opcion, double valor){
+        switch (opcion){
+            case "COP a Dólares":
+                JOptionPane.showMessageDialog(null, valor + " pesos colombianos es igual a " + extraerInformacion("COP-USD")*valor + " dolares");
+                break;
+            case "COP a Euros":
+                JOptionPane.showMessageDialog(null, valor + " pesos colombianos es igual a " + extraerInformacion("COP-EUR")*valor + " euros");
+                break;
+            case "COP a Libras Esterlinas":
+                JOptionPane.showMessageDialog(null, valor + " pesos colombianos es igual a " + extraerInformacion("COP-GBP")*valor + " libras esterlinas");
+                break;
+            case "COP a Yen japonés":
+                JOptionPane.showMessageDialog(null, valor + " pesos colombianos es igual a " + extraerInformacion("COP-JPY")*valor + " yenes japoneses");
+                break;
+            case "COP a Won Sur-coreano":
+                JOptionPane.showMessageDialog(null, valor + " pesos colombianos es igual a " + extraerInformacion("COP-KRW")*valor + " wones sur-coreano");
+                break;
+            case "Doláres a COP":
+                JOptionPane.showMessageDialog(null, valor + " doláres es igual a " + extraerInformacion("USD-COP")*valor + " pesos colombianos");
+                break;
+            case "Euros a COP":
+                JOptionPane.showMessageDialog(null, valor + " euros es igual a " + extraerInformacion("EUR-COP")*valor + " pesos colombianos");
+                break;
+            case "Libras Esterlinas a COP":
+                JOptionPane.showMessageDialog(null, valor + " libras esterlinas es igual a " + extraerInformacion("GBP-COP")*valor + " pesos colombianos");
+                break;
+            case "Yen japónes a COP":
+                JOptionPane.showMessageDialog(null, valor + " yenes japoneses es igual a " + extraerInformacion("JPY-COP")*valor + " pesos colombianos");
+                break;
+            case "Won Sur-coreano a COP":
+                JOptionPane.showMessageDialog(null, valor + " wones sur-coreano es igual a " + extraerInformacion("KRW-COP")*valor + " pesos colombianos");
+                break;
+        }
+    }
+
+    private static double extraerInformacion(String aConvertir){
+        String pagina = "https://www.google.com/finance/quote/" + aConvertir;
+
         Connection conexion = Jsoup.connect(pagina);
-        //executing the get request
+
         Document documento = null;
         try {
             documento = conexion.get();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            JOptionPane.showMessageDialog(null, "Ha ocurrido un error", "Error",JOptionPane.ERROR_MESSAGE);
+
+            e.printStackTrace();
         }
         //Retrieving the contents (body) of the web page
         Element resultado = documento.body();
 
         Elements valor = resultado.getElementsByClass("YMlKec fxKbKc");
 
-        System.out.println("USD a COP: " + valor.html());
+        return Double.parseDouble(valor.html());
+    }
+
+    private static void temperatura(double valor){
+        Object[] conversiones = {"Grado Celcius a Fahrenheit", "Grado"};
+
+        Object opcionesDeConvertidor = JOptionPane.showInputDialog(null,"Seleccione a que moneda convertir", "Conversion de Monedas", JOptionPane.INFORMATION_MESSAGE, null, conversiones, conversiones[0]);
+    }
+
+    private static boolean deseaSeguir(){
+        int seguir = JOptionPane.showInternalConfirmDialog(null,                 "¿Desea continuar?", "Seguir convirtiendo", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+
+        switch (seguir){
+            case 0:
+                inicio();
+                break;
+            case 1:
+                JOptionPane.showMessageDialog(null, "Hasta pronto", "Cerrar",JOptionPane.INFORMATION_MESSAGE,null);
+                break;
+            default:
+                break;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        inicio();
     }
 }
---
+++ Challenges/Challenge ONE Conversor de Monedas/src/Prueba.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+
+public class Prueba {
+    public static void main(String[] args) {
+    }
+}
